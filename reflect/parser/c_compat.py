1a09e60c11b5c3f8238669c33525e1f5f67aab9d
Cédric Legrand <lefta@reload.systems>
2017-11-23 23:01:53 +0100

    ADD: parser: C include path support
    Both CFLAGS provided and builtin. This uses clang, but should work fine
    in all cases.

--- reflect/parser/c_compat.py
+++ reflect/parser/c_compat.py
@@ -21,11 +21,64 @@
 
 import os
 import logging
+import subprocess
+from argparse import ArgumentParser
 from clang.cindex import Index, TranslationUnitLoadError
 from reflect.parser.error import ParseError
 from reflect.parser import ast
 
 include_dirs = []
+
+# Add CFLAGS environment variable include dirs
+parser = ArgumentParser()
+parser.add_argument('-I', dest='dirs', default=[], action='append')
+try:
+  args, unknown = parser.parse_known_args(os.environ['CFLAGS'].split())
+  for d in args.dirs:
+    include_dirs += d
+except KeyError:
+  # Silently continue if there is no CFLAGS environment variable
+  pass
+
+
+try:
+  # Run clang without input in verbose mode just to get its default include directories to have an
+  # environment as close to upcoming build as possible. There will be differences do it like this,
+  # but:
+  # - We know we have clang as we rely on its library for parsing. At least it becomes a minor
+  #   dependency.
+  # - It is ways easier and more reliable than supporting each and every system / (cross) compiler
+  #   combo out there
+  # - They should be quite the same than the ones actually used
+  # - Only differences should be minor / internal enough to not have consequences on reflect code
+  proc = subprocess.run(['clang', '-E', '-v', '-'], stdin=subprocess.PIPE,
+    stderr=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8')
+
+  if proc.returncode != 0:
+    # Just to stop the execution of the try block
+    raise Exception('')
+
+  i1 = i2 = None
+  lines = proc.stderr.split('\n')
+  for i, l in enumerate(lines):
+    if l == '#include "..." search starts here:':
+      i1 = i
+    elif l == 'End of search list.':
+      i2 = i
+
+  # Should not happen unless clang changes its output, which is very unlikely
+  assert i1 is not None and i2 is not None and i1 < i2
+  lines = lines[i1+1:i2]
+  lines.remove('#include <...> search starts here:')
+  include_dirs += [l.strip() for l in lines]
+
+except Exception:
+  logging.warning('failed to get default include directories')
+
+
+# Yups, mixing multiple languages in the same directory would be very disapointing, but who knows...
+include_dirs.append('.')
+
 
 def cursor_to_reflect(cursor):
   ast = []
