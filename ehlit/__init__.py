116ea5a9c0c95d449c5a7d4a46c66bc0f0f88980
Cédric Legrand <cedric@hackncode.tech>
2018-08-06 21:33:14 +0200

    Upd: Dump the AST before raising the errors

--- ehlit/__init__.py
+++ ehlit/__init__.py
@@ -38,11 +38,11 @@ def build(args):
   except ParseError as err:
     failure = err
 
-  if failure is not None and failure.max_level > ParseError.Severity.Warning:
-    raise failure
-
   if args.verbose:
     WriteDump(ast)
+
+  if failure is not None and failure.max_level > ParseError.Severity.Warning:
+    raise failure
 
   WriteSource(ast, args.output_file)
   WriteImport(ast, args.output_import_file)
