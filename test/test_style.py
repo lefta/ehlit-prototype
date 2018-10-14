53cc7c836d9ad5534206ce640c98db0e3d616457
Cédric Legrand <cedric@hackncode.tech>
2018-10-14 19:40:31 +0200

    Fix: test: Style test never reporting errors

--- test/test_style.py
+++ test/test_style.py
@@ -1,4 +1,5 @@
 from flake8.api import legacy as flake8
+from glob import glob
 import unittest
 
 
@@ -8,7 +9,6 @@ class TestCodeFormat(unittest.TestCase):
 
         style_guide = flake8.get_style_guide(quiet=2, ignore=['E111', 'E114', 'E121', 'W191',
                                                               'E266', 'E402'])
-        report = style_guide.check_files('..')
-        errors = (len(report.get_statistics('F')) + len(report.get_statistics('E')) +
-                  len(report.get_statistics('W')))
-        self.assertEqual(errors, 0, 'Flake8 found {} violations'.format(errors))
+        report = style_guide.check_files(glob('../**/*.py', recursive=True))
+        self.assertEqual(report.total_errors, 0,
+                         'Flake8 found {} violations'.format(report.total_errors))
