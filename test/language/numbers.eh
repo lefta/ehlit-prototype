99b65cf83ef0cfba2d50e748a2f0eb6a11a27057
Cédric Legrand <cedric@hackncode.tech>
2018-10-26 20:02:02 +0200

    Add: test: Integer builtin types

--- test/language/numbers.eh
+++ test/language/numbers.eh
@@ -1,4 +1,13 @@
 int main() {
+  int8 i8
+  int16 i16
+  int32 i32
+  int64 i64
+  uint8 ui8
+  uint16 ui16
+  uint32 ui32
+  uint64 ui64
+
   int i = 42
   int j = -42
 
--- test/language/numbers.eh.c
+++ test/language/numbers.eh.c
@@ -3,6 +3,14 @@
 
 int32_t main(void)
 {
+    int8_t i8;
+    int16_t i16;
+    int32_t i32;
+    int64_t i64;
+    uint8_t ui8;
+    uint16_t ui16;
+    uint32_t ui32;
+    uint64_t ui64;
     int32_t i = 42;
     int32_t j = -42;
     return (0);
